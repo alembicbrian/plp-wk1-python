95343945354f8245cd2b948d333911c7e5b4dcb8
alembicbrian <alembicbrian@gmail.com>
2024-02-26 01:09:09 +0300

    user input request and output

---
+++ user_input.py
@@ -0,0 +1,4 @@
+name= input("Enter your name:")
+age=input("Enter your age:")
+location=input("Whats your location?")
+print("Hello"+" "+name+" "+"please confirm that you are"+" "+age+" "+"years and you are from"+" "+location+".")
